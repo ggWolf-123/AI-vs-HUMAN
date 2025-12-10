9f002bbb16cd116f7f62e44cb4613064e191cd91
ggWOLF <zdybjan8@gmail.com>
2025-12-10 18:48:51 +0100

    Dodaj pliki projektów.

---
+++ AI vs HUMAN/mini_gra.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AI_vs_HUMAN
+{
+    public partial class mini_gra : Form
+    {
+        public mini_gra()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AI vs HUMAN/test_obrazu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AI_vs_HUMAN
+{
+    public partial class test_obrazu : Form
+    {
+        public test_obrazu()
+        {
+            InitializeComponent();
+        }
+    }
+}
